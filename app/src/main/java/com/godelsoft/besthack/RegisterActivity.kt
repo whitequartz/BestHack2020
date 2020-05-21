cad474202b271f04a6d533a76ea896a1d1269517
Ilya Karkin <mitbus@mail.ru>
2020-05-21 18:23:07 +0300

    login form added

---
+++ app/src/main/java/com/godelsoft/besthack/RegisterActivity.kt
@@ -0,0 +1,35 @@
+package com.godelsoft.besthack
+
+import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.clearTask
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.newTask
+
+class RegisterActivity : AppCompatActivity()   {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        textErrorEMail.visibility = INVISIBLE
+        textErrorPassword.visibility = INVISIBLE
+
+        buttonRegister.setOnClickListener {
+            textErrorEMail.visibility = INVISIBLE
+            textErrorPassword.visibility = INVISIBLE
+
+            val loginExsist = false  // email лежит в editTextTextEmailAddress.text
+            if(loginExsist) {
+                textErrorEMail.visibility = VISIBLE
+            } else if(editTextTextPassword.text.toString() != editTextTextPassword2.text.toString()) {
+                textErrorPassword.visibility = VISIBLE
+            } else {
+                // create new account
+                startActivity(intentFor<MainActivity>().newTask().clearTask())
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/godelsoft/besthack/SignInActivity.kt
@@ -0,0 +1,32 @@
+package com.godelsoft.besthack
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import org.jetbrains.anko.*
+import java.lang.Exception
+
+class SignInActivity : AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        buttonLogin.setOnClickListener {
+            var signedIn = false
+            try {
+                signedIn = true  // Запрос лог: textEmailAddress.text пароль: textPassword.text
+            } catch (e: Exception) {  }
+
+            if (signedIn) {
+                startActivity(intentFor<MainActivity>().newTask().clearTask())
+            } else {
+
+            }
+        }
+
+        buttonCreateNew.setOnClickListener {
+            startActivity(intentFor<RegisterActivity>().newTask().clearTask())
+        }
+    }
+}
