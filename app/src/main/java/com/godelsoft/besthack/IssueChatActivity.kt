bb8de04c237f17630dd7bb11e8419dc735b41ff9
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-05-22 00:03:09 +0300

    added human messages

--- app/src/main/java/com/godelsoft/besthack/IssueChatActivity.kt
+++ app/src/main/java/com/godelsoft/besthack/IssueChatActivity.kt
@@ -2,10 +2,16 @@ package com.godelsoft.besthack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.TransitionManager
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.godelsoft.besthack.recycleViewAdapters.MessageAdapter
+import kotlinx.android.synthetic.main.activity_issue_chat.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.recycleView
 import org.jetbrains.anko.startActivity
+import java.util.*
 
 class IssueChatActivity : AppCompatActivity() {
     lateinit var recycleAdapter: MessageAdapter
@@ -43,95 +49,6 @@ class IssueChatActivity : AppCompatActivity() {
             bot.getMessage("request"),
             bot.getMessage("support"))))
 
-//        recycleAdapter.add(Message.selectMessages(recycleAdapter,
-//            Message(User.current, "1", "sss") {
-////                recycleAdapter.add(Message.selectMessages(recycleAdapter,
-////                            Message(User.current, "Точно!", "2") {
-////                                recycleAdapter.add(listOf(Message(ts, "ok", "228")))
-////                            },
-////                            Message(User.current, "Нет", "2")))
-//                recycleAdapter.add(mutableListOf(Message(ts, "Точно 1?", "sss")).apply {
-//                    addAll(Message.selectMessages(recycleAdapter,
-//                            Message(User.current, "Точно!", "2") {
-//                                recycleAdapter.add(listOf(Message(ts, "ok", "228")))
-//                            },
-//                            Message(User.current, "Нет", "2")
-//                        ))
-//                })
-//            },
-//            Message(User.current, "2", "sss") {
-//                startActivity<LoginActivity>()
-//            }
-//        ))
-
-        val connectToSupport = fun() {
-
-        }
-
-//        var f = {}
-//        f = {
-//            recycleAdapter.add(Message.selectMessages(recycleAdapter,
-//                Message(User.current, "Lorem 1", "time") {
-//                    Toast.makeText(MainActivity.mainContext, "Lorem 1", Toast.LENGTH_SHORT).show()
-//                    f()
-//                },Message(User.current, "Lorem 2", "time") {
-//                    Toast.makeText(MainActivity.mainContext, "Lorem 2", Toast.LENGTH_SHORT).show()
-//                    f()
-//                },Message(User.current, "Lorem 3", "time") {
-//                    Toast.makeText(MainActivity.mainContext, "Lorem 3", Toast.LENGTH_SHORT).show()
-//                    f()
-//                },Message(User.current, "Lorem 4", "time") {
-//                    Toast.makeText(MainActivity.mainContext, "Lorem 4", Toast.LENGTH_SHORT).show()
-//                    f()
-//                },Message(User.current, "Lorem 5", "time") {
-//                    Toast.makeText(MainActivity.mainContext, "Lorem 5", Toast.LENGTH_SHORT).show()
-//                    f()
-//                },Message(User.current, "Lorem 6", "time") {
-//                    Toast.makeText(MainActivity.mainContext, "Lorem 6", Toast.LENGTH_SHORT).show()
-//                    f()
-//                },Message(User.current, "Lorem 7", "time") {
-//                    Toast.makeText(MainActivity.mainContext, "Lorem 7", Toast.LENGTH_SHORT).show()
-//                    f()
-//                }
-//            ))
-//        }
-//
-//        recycleAdapter.update(
-//            mutableListOf(
-//            Message(User.current, "text", "time"),
-//            Message(User.current, "text", "time"),
-//            Message(User.current, "text", "time"),
-//            Message(ts, "text", "time"),
-//            Message(ts, "text", "time"),
-//            Message(User.current, "text", "time"),
-//            Message(ts, "text", "time"),
-//            Message(ts, "text", "time"),
-//            Message(ts, "text", "time"),
-//            Message(User.current, "text", "time")
-//        ).apply { addAll(Message.selectMessages(recycleAdapter,
-//                Message(User.current, "Lorem 1", "time") {
-//                    Toast.makeText(MainActivity.mainContext, "Lorem 1", Toast.LENGTH_SHORT).show()
-//                    f()
-//                },Message(User.current, "Lorem 2", "time") {
-//                    Toast.makeText(MainActivity.mainContext, "Lorem 2", Toast.LENGTH_SHORT).show()
-//                    f()
-//                },Message(User.current, "Lorem 3", "time") {
-//                    Toast.makeText(MainActivity.mainContext, "Lorem 3", Toast.LENGTH_SHORT).show()
-//                    f()
-//                },Message(User.current, "Lorem 4", "time") {
-//                    Toast.makeText(MainActivity.mainContext, "Lorem 4", Toast.LENGTH_SHORT).show()
-//                    f()
-//                },Message(User.current, "Lorem 5", "time") {
-//                    Toast.makeText(MainActivity.mainContext, "Lorem 5", Toast.LENGTH_SHORT).show()
-//                    f()
-//                },Message(User.current, "Lorem 6", "time") {
-//                    Toast.makeText(MainActivity.mainContext, "Lorem 6", Toast.LENGTH_SHORT).show()
-//                    f()
-//                },Message(User.current, "Lorem 7", "time") {
-//                    Toast.makeText(MainActivity.mainContext, "Lorem 7", Toast.LENGTH_SHORT).show()
-//                    f()
-//                }
-//                )) })
 
         recycleView.apply {
             layoutManager = LinearLayoutManager(MainActivity.main).apply {
