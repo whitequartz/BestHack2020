6d5698c48b51038929a13b1982b4317423a7c8c1
Sokolovskiy Mikhail <sokolmish@yandex.ru>
2020-05-21 15:17:14 +0300

    More correct network

--- app/src/main/java/com/godelsoft/besthack/MainActivity.kt
+++ app/src/main/java/com/godelsoft/besthack/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.godelsoft.besthack
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import java.io.*
 import java.net.InetAddress
 import java.net.Socket
@@ -12,50 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val test = Test()
-        Thread(test).start()
-    }
-}
 
-class Test : Runnable {
-    private var port = 8081 // Порт, такой же, как у сервера
-    private var address = "127.0.0.1" // Адрес сервера
-
-    override fun run() {
-            try {
-                // Преобразуем адрес из строки во внутреннее представление
-                val addr: InetAddress = InetAddress.getByName(address)
-
-                // Создаём сокет и подключаем его к серверу
-                println("Поключаемся к $address:$port...")
-                val socket = Socket(addr, port)
-
-                // Потоки ввода/вывода
-                // BufferedReader позволяет читать вход по строкам
-                val `in` = BufferedReader(InputStreamReader(socket.getInputStream()))
-                // PrintWriter позволяет использовать println
-                val out = PrintWriter(OutputStreamWriter(socket.getOutputStream(), "UTF-8"))
-                // Создаем объект для чтения строк с клавиатуры
-                val scan = Scanner(System.`in`, "UTF-8")
-
-                // Читаем данные с клавиатуры и отправляем серверу
-                // Запрашиваем строку
-                print("[Запрос]:")
-                var line: String = scan.nextLine()
-
-                // Отправляем строку серверу
-                out.println(line)
-                out.flush() // принудительная отправка
-
-                // Получаем ответ
-                line = `in`.readLine() // ждем пока сервер отошлет строку текста.
-                println("[Ответ]:$line")
-
-                // Закрываем соединение
-                socket.close()
-            } catch (x: IOException) {
-                println("Ошибка ввода/вывода")
-                x.printStackTrace()
-            }
+        button_secret.setOnClickListener {
+            startActivity(Intent(this, SecretActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/godelsoft/besthack/SecretActivity.kt
+++ app/src/main/java/com/godelsoft/besthack/SecretActivity.kt
@@ -1,8 +1,15 @@
 package com.godelsoft.besthack
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.SyncStateContract
+import androidx.appcompat.app.AppCompatActivity
+//import com.sun.xml.internal.ws.streaming.XMLStreamWriterUtil.getOutputStream
+//import jdk.nashorn.internal.runtime.ScriptingFunctions.readLine
 import kotlinx.android.synthetic.main.activity_secret.*
+import java.io.*
+import java.net.InetAddress
+import java.net.Socket
+
 
 class SecretActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,11 +17,48 @@ class SecretActivity : AppCompatActivity() {
         setContentView(R.layout.activity_secret)
 
         button_test_1.setOnClickListener {
-            response_testbox.setText("TESTTESTTESTTEST")
+            val test = Test { res ->
+                runOnUiThread {
+                    response_testbox.setText(res)
+                }
+            }
+            Thread(test).start()
         }
 
         button_test_2.setOnClickListener {
             response_testbox.setText("2222222 2222 2222222 22222222")
+        }
+    }
+}
+
+class Test(private val callback: (String) -> Unit) : Runnable {
+    private var port = 8081
+    private var address = "10.0.2.2"
+
+    override fun run() {
+        try {
+            val addr = InetAddress.getByName(address)
+
+            println("Поключаемся к $addr:$port...")
+            val socket = Socket(addr, port)
+
+            val `in` = BufferedReader(InputStreamReader(socket.getInputStream()))
+            val out = PrintWriter(OutputStreamWriter(socket.getOutputStream(), "UTF-8"))
+
+            print("[Запрос]:")
+            var line = "AlPHa\n"
+
+            out.println(line)
+            out.flush()
+
+            line = `in`.readLine()
+            println("[Ответ]:$line")
+            callback(line)
+
+            socket.close()
+        } catch (x: IOException) {
+            println("Ошибка ввода/вывода")
+            x.printStackTrace()
         }
     }
 }
