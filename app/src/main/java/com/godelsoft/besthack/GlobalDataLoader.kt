e7af298a5968d931e7aeed71b36f4788a2c1b1ad
Ilya Karkin <mitbus@mail.ru>
2020-05-22 04:22:39 +0300

    save chat

--- app/src/main/java/com/godelsoft/besthack/GlobalDataLoader.kt
+++ app/src/main/java/com/godelsoft/besthack/GlobalDataLoader.kt
@@ -1,7 +1,7 @@
 package com.godelsoft.besthack
 
 object GlobalDataLoader {
-//    var serverIP = "34.69.134.32"   // Nadim
-    var serverIP = "10.0.2.2"       // Local
+    var serverIP = "34.69.134.32"   // Nadim
+//    var serverIP = "10.0.2.2"       // Local
     var serverPort = 8081
 }
